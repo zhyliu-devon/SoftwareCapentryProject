65795e15a4edaf1c163ebac44b6c0d73cff7b085
unknown <1155141684@link.cuhk.edu.hk>
2024-11-05 11:25:25 -0500

    created solve board function with permutation: too slow

--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
 from classes import *
 import os
+import math
+import itertools
 
 def read_and_visualize_board(filename: str) -> LazorGame:
     """
@@ -29,23 +31,51 @@ def solve_board(game: LazorGame) -> bool:
         bool: True if a solution was found, False otherwise
     """
     # 1 Get all possible positions for blocks   
+    available_positions = []
+    for y in range(len(game.grid)):
+        for x in range(len(game.grid[0])):
+            if game.grid[y][x] == 'o':
+                available_positions.append((x, y))
+    
     # 2 For each block type, get the number of blocks needed
     blocks_needed = []
+    for block_type, count in game.blocks.items():
+        blocks_needed.extend([block_type] * count)
 
     # If no blocks needed, check if current configuration works
-
+    if not blocks_needed:
+        game.propogate()
+        return game.validate()
     
     # 3 Get all possible combinations of positions for the blocks
-
+    num_positions_needed = len(blocks_needed)
+    if num_positions_needed > len(available_positions):
+        return False
     # 4 Calculate total number of combinations to try
-
+    n_c = math.comb(len(available_positions), num_positions_needed)
+    n_p = math.factorial(num_positions_needed)
+    total_it = n_c * n_p
+    
+    print(f"In total {total_it:,} possibilities")
+    
     # 5 Try each permutation of block types in these positions
+    for positions in itertools.combinations(available_positions, num_positions_needed):
+        for block_arrangement in itertools.permutations(blocks_needed):
 
-    # 6 Create a copy of the game to test this configuration
+            # 6 Create a copy of the game to test this configuration
+            test_game = LazorGame(game.filename)
 
-    # 7 Place blocks according to this arrangement
- 
+            # 7 Place blocks according to this arrangement
+            valid = True
+            for (x, y), block_type in zip(positions, block_arrangement):
+                if not test_game.add_block(block_type, (x, y)):
+                    valid = False
+                    break
     # 8 check if this is a solution
-  
+            test_game.propogate()
+            if test_game.validate():
     # 9 If solution found, apply it to the original game
-    pass
+                for (x, y), block_type in zip(positions, block_arrangement):
+                    game.add_block(block_type, (x, y))
+                return True
+    return False
